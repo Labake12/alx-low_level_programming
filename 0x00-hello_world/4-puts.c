9db0a84d79186e7f4fa4de01cafdceb0d0ad65e8
Labake12 <labakeadegoroye@gmail.com>
2022-09-08 16:59:23 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * print the string in the put function
+ *
+ * using the main function
+ * this program should print "Programming is like building a multilingual puzzle
+ * Return: 0
+ *
+ * */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
