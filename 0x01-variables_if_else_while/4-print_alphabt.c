b82f5990045037564f6782797a693554b8ca2cb8
Labake12 <labakeadegoroye@gmail.com>
2022-09-09 18:35:14 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,15 +6,20 @@
  */
 int main(void)
 {
-	char alphabet;
+	char alphabet
+		;
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		if (alphabet == 'q')
+
 			continue;
 		else if (alphabet == 'e')
+
 			continue;
 		putchar(alphabet);
 	}
+
 	putchar('\n');
+
 	return (0);
 }
