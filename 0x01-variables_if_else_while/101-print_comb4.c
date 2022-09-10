3059fee864da53e966677465166f1fe5e2f95531
Labake12 <labakeadegoroye@gmail.com>
2022-09-09 20:39:03 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int first;
+	int second;
+	int third;
+
+	for (first = 48; first <= 57; first++)
+	{
+		(second = first + 1; second <= 57; second++)
+		{
+			(third = second + 1; third <= 57; third++)
+			{
+				putchar(first);
+				putchar(second);
+				putchar(third);
+
+				if ((first == 55) && (second == first + 1) && (third == second + 1))
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
