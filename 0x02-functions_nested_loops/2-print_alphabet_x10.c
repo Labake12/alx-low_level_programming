b24079506879da36314f83057330c50c8722505d
Labake12 <labakeadegoroye@gmail.com>
2022-09-13 17:40:27 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** print_alphabet_x10 - a function the alphabet 10 times
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+	char i, l;
+
+	for (l = 0; l <= 9; l++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
