045e647573425511243f7f633dbb8dc5801d1c64
Labake12 <labakeadegoroye@gmail.com>
2022-09-15 14:24:18 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,15 +6,30 @@
  */
 void print_number(int n)
 {
+	unsigned int num, num1;
+	int i;
+	int c = 1;
+
 	if (n < 0)
 	{
+		n = n * -1;
 		_putchar('-');
-		n = -n;
 	}
-	if (n / 10 != 0)
+	num = n;
+	num1 = num;
+	if (num > 9)
 	{
-		print_number(n / 10);
-		break;
+		while (num >= 10)
+		{
+			c = c * 10;
+			num = num / 10;
+		}
+		_putchar((num1 / c) + '0');
+		c = c / 10;
+
+		for (i = c; i >= 1; i = i / 10)
+			_putchar((num1 / i) % 10 + '0')
 	}
-	_putchar(n % 10 + '0');
+	else
+		_putchar(num + '0');
 }
