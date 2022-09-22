fd10d24a1393f753b2244c5f27591e4ab5f0b303
Labake12 <labakeadegoroye@gmail.com>
2022-09-22 14:14:35 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -21,7 +21,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (*s2 != 0)
 		s2++;
 	size_r--;
-	r[soze_r] = 0;
+	r[size_r] = 0;
 	s1--;
 	s2--;
 	while (s2 != n2 - 1 && s1 != n1 - 1)
@@ -33,9 +33,24 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			c++;
 			r[index] -= 10;
 		}
+		index++;
+		s2--;
+		s1--;
+		if (size_r == index && (s1 != n1 - 1 || s2 != n2 - 1 || c == 1))
+			return (0);
+	}
+	while (s1 != n1 - 1)
+	{
+		r[index] = *s1 + c;
+		c = 0;
+		if (r[index] > '9')
+		{
+			c = 1;
+			r[index] -= 10;
+		}
 		s1--;
 		index++;
-		if (size_r == index && (s1 != n1 - 1 || c == 1))
+		if (size_r == index && (s1 != n1 - 1 ||  c == 1))
 			return (0);
 	}
 	while (s2 != n2 - 1)
