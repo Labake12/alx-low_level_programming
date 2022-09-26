ffd41daa73d664bab4db925047bca5165e36f0b6
Labake12 <labakeadegoroye@gmail.com>
2022-09-26 15:22:10 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,13 +11,15 @@ unsigned int _strspn(char *s, char *accept);
 {
 	unsigned int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; accept[j] != s[i]; j++)
+		for (j = 0; accept[j]; j++)
 		{
-			if (accept[j] == '\0')
-				return (i);
+			if (s[i] == accept[j])
+				break;
 		}
+		if (!accept[j])
+			break;
 	}
 	return (i);
 }
